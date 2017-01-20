9471748226452c78f7de109ac4f06e91d48d3b06
Grant Nestor <grantnestor@gmail.com>
2017-01-20 08:32:23 -0800

    Inject static image into output mime bundle

--- labextension/src/output.js
+++ labextension/src/output.js
@@ -1,7 +1,7 @@
 import { Widget } from 'phosphor/lib/ui/widget';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Vega, VegaLite } from 'jupyterlab_vega_react';
+import Vega from 'jupyterlab_vega_react';
 
 /**
  * The class name added to this OutputWidget.
@@ -18,6 +18,7 @@ class OutputWidget extends Widget {
     this.addClass(CLASS_NAME);
     this._source = options.source;
     this._mimetype = options.mimetype;
+    this._injector = options.injector;
   }
 
   /**
@@ -40,10 +41,17 @@ class OutputWidget extends Widget {
   _render() {
     const json = this._source;
     const mimetype = this._mimetype;
-    ReactDOM.render(
-      mimetype === 'application/vnd.vegalite+json' ? <VegaLite data={json} /> : <Vega data={json} />,
-      this.node
-    );
+    const props = {
+      data: json,
+      embedMode: mimetype === 'application/vnd.vegalite.v1+json' ? 'vega-lite' : 'vega',
+      renderedCallback: (error, result) => {
+        // Add a static image output to mime bundle
+        if (error) return console.log(error);
+        const imageData = result.view.toImageURL().split(',')[1];
+        this._injector('image/png', imageData);
+      }
+    };
+    ReactDOM.render(<Vega {...props} />, this.node);
   }
 
 }
@@ -53,6 +61,7 @@ export class VegaOutput {
   /**
    * The mime types this OutputRenderer accepts.
    */
+  mimetypes = [ 'application/vnd.vega.v2+json' ];
 
   /**
    * Whether the input can safely sanitized for a given mime type.
@@ -82,6 +91,7 @@ export class VegaLiteOutput {
   /**
    * The mime types this OutputRenderer accepts.
    */
+  mimetypes = [ 'application/vnd.vegalite.v1+json' ];
 
   /**
    * Whether the input can safely sanitized for a given mime type.
--- nbextension/src/renderer.js
+++ nbextension/src/renderer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Vega, VegaLite } from 'jupyterlab_vega_react';
+import Vega from 'jupyterlab_vega_react';
 import './index.css';
 
 const VEGA_MIME_TYPE = 'application/vnd.vega.v2+json';
@@ -10,12 +10,8 @@ const CLASS_NAME = 'output_Vega rendered_html';
 //
 // Render data to the output area
 // 
-function render_vega(data, node) {
-  ReactDOM.render(<Vega data={data} />, node);
-}
-
-function render_vegalite(data, node) {
-  ReactDOM.render(<VegaLite data={data} />, node);
+function render(props, node) {
+  ReactDOM.render(<Vega {...props} />, node);
 }
 
 //
@@ -23,23 +19,31 @@ function render_vegalite(data, node) {
 // 
 export function register_renderer($) {
   // Get an instance of the OutputArea object from the first CodeCellebook_
-  const OutputArea = $('#notebook-container').find('.code_cell').eq(0).data('cell').output_area;
-  // A function to render output of 'application/vnd.vega+json' mime type
-  function append_vega(json, md, element) {
-    const toinsert = this.create_output_subarea(md, CLASS_NAME, VEGA_MIME_TYPE);
-    this.keyboard_manager.register_events(toinsert);
-    render_vega(json, toinsert[0]);
-    element.append(toinsert);
-    return toinsert;
-  };
-  // A function to render output of 'application/vnd.vegalite+json' mime type
-  function append_vegalite(json, md, element) {
-    const toinsert = this.create_output_subarea(md, CLASS_NAME, VEGALITE_MIME_TYPE);
-    this.keyboard_manager.register_events(toinsert);
-    render_vegalite(json, toinsert[0]);
-    element.append(toinsert);
-    return toinsert;
-  };
+  const OutputArea = $('#notebook-container')
+    .find('.code_cell')
+    .eq(0)
+    .data('cell').output_area;
+  // A function to render output of Vega mime type
+  function append_mime(mimetype) {
+    const embedMode = mimetype === 'application/vnd.vegalite.v1+json' ? 'vega-lite' : 'vega'
+    return function(json, md, element) {
+      const toinsert = this.create_output_subarea(md, CLASS_NAME, mimetype);
+      this.keyboard_manager.register_events(toinsert);
+      const props = {
+        data: json,
+        embedMode,
+        renderedCallback: (error, result) => {
+          if (error) return console.log(error);
+          // Add a static image output to mime bundle
+          const imageData = result.view.toImageURL().split(',')[1];
+          if (this.outputs[0]) this.outputs[0].data['image/png'] = imageData;
+        }
+      };
+      render(props, toinsert[(0)]);
+      element.append(toinsert);
+      return toinsert;
+    }
+  }
   // Calculate the index of this renderer in `OutputArea.display_order`
   // e.g. Insert this renderer after any renderers with mime type that matches "+json"
   // const mime_types = OutputArea.mime_types();
@@ -48,17 +52,13 @@ export function register_renderer($) {
   // ...or just insert it at the top
   const index = 0;
   // Register the mime type and append_mime_type function with the notebook's OutputArea
-  OutputArea.register_mime_type(VEGA_MIME_TYPE, append_vega, {
-    // Is output safe?
-    safe: true,
-    // Index of renderer in `OutputArea.display_order`
-    index: index
-  });
-  OutputArea.register_mime_type(VEGALITE_MIME_TYPE, append_vegalite, {
-    // Is output safe?
-    safe: true,
-    // Index of renderer in `OutputArea.display_order`
-    index: index
+  [VEGA_MIME_TYPE, VEGALITE_MIME_TYPE].forEach(mimetype => {
+    OutputArea.register_mime_type(mimetype, append_mime(mimetype), {
+      // Is output safe?
+      safe: true,
+      // Index of renderer in `OutputArea.display_order`
+      index: index
+    });
   });
 }
 
@@ -69,8 +69,15 @@ export function render_cells($) {
   // Get all cells in notebook
   $('#notebook-container').find('.cell').toArray().forEach(item => {
     const CodeCell = $(item).data('cell');
-    // If a cell has output data of 'application/vnd.vega+json' mime type
-    if (CodeCell.output_area && CodeCell.output_area.outputs.find(output => output.data && (output.data[VEGA_MIME_TYPE] || output.data[VEGALITE_MIME_TYPE]))) {
+    // If a cell has output data of 'application/vnd.vega.v2+json' mime type
+    if (
+      CodeCell.output_area &&
+        CodeCell.output_area.outputs.find(
+          output =>
+            output.data &&
+              (output.data[VEGA_MIME_TYPE] || output.data[VEGALITE_MIME_TYPE])
+        )
+    ) {
       // Re-render the cell by executing it
       CodeCell.notebook.render_cell_output(CodeCell);
     }
