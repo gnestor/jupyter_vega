0dfaa2443a246ce10f72306ab0e1995dc5c9e9a2
Grant Nestor <grantnestor@gmail.com>
2017-04-10 15:05:17 -0700

    mimerender-cookiecutter 0.18.0

--- component/index.js
+++ component/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './index.css';
 
 export default class VegaComponent extends React.Component {
-
   render() {
-      return (
-        <div className="Vega">
-          {JSON.stringify(this.props.data)}
-        </div>
-      );
+    return (
+      <div className="Vega">
+        {JSON.stringify(this.props)}
+      </div>
+    );
   }
-
 }
--- jupyterlab_vega/__init__.py
+++ jupyterlab_vega/__init__.py
@@ -1,10 +1,7 @@
 from IPython.display import display, JSON
-import json
-
 
 # Running `npm run build` will create static resources in the static
 # directory of this Python package (and create that directory if necessary).
-
 
 def _jupyter_labextension_paths():
     return [{
@@ -20,8 +17,7 @@ def _jupyter_nbextension_paths():
         'require': 'jupyterlab_vega/extension'
     }]
 
-
-# A display class that can be used within a notebook. E.g.:
+# A display class that can be used within a notebook. 
 #   from jupyterlab_vega import Vega
 #   Vega(data)
     
@@ -33,19 +29,6 @@ class Vega(JSON):
     Scalar types (None, number, string) are not allowed, only dict containers.
     """
 
-    # @property
-    # def data(self):
-    #     return self._data
-    # 
-    # @data.setter
-    # def data(self, data):
-    #     if isinstance(data, str):
-    #         data = json.loads(data)
-    #     self._data = data
-
-    def _data_and_metadata(self):
-        return self.data, self.metadata
-    
     def _ipython_display_(self):
         bundle = {
             'application/vnd.vega.v2+json': self.data,
